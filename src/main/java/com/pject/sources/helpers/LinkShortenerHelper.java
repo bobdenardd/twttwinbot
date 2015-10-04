8999872b78c45006e4a44d39c800780d833f7ff7
Camille Espanol <espanol@fullsix.com>
2015-10-04 14:10:32 +0200

    Adding the link shortener helper (only goo.gl for now)

---
+++ src/main/java/com/pject/sources/helpers/LinkShortenerHelper.java
@@ -0,0 +1,11 @@
+package com.pject.sources.helpers;
+
+/**
+ * LinkShortenerHelper - Short description of the class
+ *
+ * @author Camille
+ *         Last: 04/10/2015 13:02
+ * @version $Id$
+ */
+public class LinkShortenerHelper {
+}
