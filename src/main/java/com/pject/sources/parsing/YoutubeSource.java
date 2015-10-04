0e62e5f2c76833e27c26ba6edabd3d6163f021dd
Camille Espanol <espanol@fullsix.com>
2015-10-04 21:52:51 +0200

    Adding the youtube source

---
+++ src/main/java/com/pject/sources/parsing/YoutubeSource.java
@@ -0,0 +1,11 @@
+package com.pject.sources.parsing;
+
+/**
+ * YoutubeSource - Short description of the class
+ *
+ * @author Camille
+ *         Last: 04/10/2015 20:54
+ * @version $Id$
+ */
+public class YoutubeSource {
+}
